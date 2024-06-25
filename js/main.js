91d21e2a39abfdd5190e88385378a49c64a63061
Nathandiv <k.n.hlungwani@gmail.com>
2024-06-25 12:07:50 +0200

    added a function add on the add button

--- js/main.js
+++ js/main.js
@@ -8,9 +8,18 @@ function readAll () {
     (elements += `<tr>
     <td> ${d.items}</td>
     <td> ${d.amount}</td>
-    <td> <button onclick="(${del()})">Delete</button></td>
+    <td> <button onclick="()">Delete</button></td>
     <tr>
   `)
 );
 tData.innerHTML = elements;
+}
+
+function add(){
+  let items = document.getElementById('items').value;
+  let amount = Number (document.getElementById('amount').value);
+
+  let newObject = { items, amount};
+  data.push(newObject);
+  readAll();
 }
